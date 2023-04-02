7af1d602a54caa9498d6bbd42cbbe78c261db8b5
Elisa Bertoni <bertoni_elisa@hotmail.com>
2023-04-02 21:23:19 +1200

    add '*' route to server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,17 @@
-import express from 'express'
 import { join } from 'node:path'
-
-import fruitRoutes from './routes/fruits'
+import path from 'node:path'
+import express from 'express'
+import wine from './routes/wine'
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(join(__dirname, 'public')))
 
-server.use('/api/v1/fruits', fruitRoutes)
+server.use('/api/v1/wine', wine)
+
+server.get('*', (req, res) => {
+  res.sendFile(path.resolve('server/public/index.html'))
+})
 
 export default server
